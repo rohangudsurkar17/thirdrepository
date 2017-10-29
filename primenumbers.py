cc6e6424649097ec26be73bd0c8e2f52a8aa4c9c
rohangudsurkar17 <33171205+rohangudsurkar17@users.noreply.github.com>
2017-10-29 12:23:05 +0530

    Update primenumbers.py
    comment added

--- primenumbers.py
+++ primenumbers.py
@@ -3,7 +3,7 @@ num = 23
 if num > 1:
     for i in range(2,num):
        if (num%i == 0):
-           print("the number is not a prime number")
+           print("the number is not a prime number") # comments added
            break
     else:
        print("the number is a prime number")
